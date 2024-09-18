69a0e27a7f217fe1c80db9e94efa490f1a1c5d6b
Jay Lathia <jaymansukh.lathia@thomsonreuters.com>
2024-09-18 22:04:49 +0530

    Added parameterized Component approach
    Added parameterized Component approach

--- TR.AngularBlazorClient/src/app/components/counter/counter.component.ts
+++ TR.AngularBlazorClient/src/app/components/counter/counter.component.ts
@@ -1,13 +1,34 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
-export class CounterComponent {
+export class CounterComponent implements AfterViewInit {
+  stepCount: number = 6;
+  counterComponent: any;
+  setDetails = {
+    myTime: new Date(),
+    userId: 48557,
+    userName: 'Jay Lathia'
+  };
 
+  ngAfterViewInit() {
+    this.setBlazorSettings();
+    // setTimeout(() => {
+    //   this.setBlazorSettings();
+    // }, 1000);
+  }
+
+  setBlazorSettings() {
+    this.counterComponent = document.querySelector('counter-component');
+    if (this.counterComponent) {
+      this.counterComponent['setDetails'] = this.setDetails;
+    }
+  }
 }
